e456f95d1ee8c3bb434bbdb48148c7f76e068fc2
Prashant Kumar <prashantdcoder@gmail.com>
2025-10-17 19:48:47 +0530

    create theme toggle component for chanign theme, write test cases

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -3,15 +3,15 @@ import useTheme from '../../hooks/useTheme';
 import { ThemeMode } from '../../utils/types';
 import { Button } from '../ui/button';
 import './ThemeToggle.css';
-import { MoonIcon, SunIcon } from 'lucide-react';
+import { Sun, Moon } from "lucide-react";
 
 const ThemeToggle: React.FC = () => {
     const { theme, toggleTheme } = useTheme();
     const buttonText: string = `Switch to ${theme === ThemeMode.DARK ? ThemeMode.LIGHT : ThemeMode.DARK} theme`;
-    const buttonIcon = theme === ThemeMode.DARK ? <SunIcon /> : <MoonIcon />;
+    const buttonIcon = theme === ThemeMode.DARK ? <Sun /> : <Moon />;
     return (
         <div className='theme-button-container'>
-            <Button className='toggle-btn' value="change theme" onClick={toggleTheme}>
+            <Button data-testid='toggle-btn' className='toggle-btn' onClick={toggleTheme}>
                 {buttonText}
                 {buttonIcon}
             </Button>
